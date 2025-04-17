951aab788ec2f6e8172ebb2b5e520f39034b66bb
Ivan Vacacela <ivacacela@yahoo.com>
2025-04-17 01:10:20 -0400

    new file hello.py

---
+++ hello.py
@@ -0,0 +1,31 @@
+class Person:
+ def __init__(self, name, age):
+  self.name = name
+  self.age = age
+p1 = Person("Ivan", 60)
+print ("name:", p1.name)
+print ("age:", p1.age)
+
+def sum(x, y):
+ return x + y
+
+print ("hello world")
+a = 1
+b = 5 
+if a > 1:
+ print ("if if")
+else:
+ print ("else else"
+
+)
+while a < b:
+ print(a)
+ a = a + 1
+
+print ("end")
+a = 1
+for a in range(b):
+ print("a", a)
+ print("b", b)
+print ("suma:",sum(4,7))
+
